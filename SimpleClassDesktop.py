a6d397b0aacdb2b81a2da891bbe11aebbab0bc33
ABsintio <32601287+ABsintio@users.noreply.github.com>
2018-02-12 19:30:31 +0100

    Update SimpleClassDesktop.py

--- SimpleClassDesktop.py
+++ SimpleClassDesktop.py
@@ -36,9 +36,8 @@ def runApp(app, window):
     app.exec_()
     
 #Create a class to define and object QWBrowser
-class QWBrowser:
-    
-    def __init__(self, window, app):
+class QWBrowser(object):
+    def __init__(self):
         app, window = BuildWindow()
         self.app = app
         self.window = window
@@ -93,5 +92,108 @@ class QWBrowser:
         web_view.urlChanged.connect(SetUrl)
         button_back.clicked.connect(web_view.back)
         button_forward.clicked.connect(web_view.forward)
+        
+    def _run_app(self):
+        self.window.show()
+        self.app.exec_()
+
+class QTEditor(object):
+    def __init__(self):
+        app, window = BuildWindow()
+        self.app = app
+        self.window = window
+    
+    def _build_editor(self):
+        self.window.setWindowTitle('Text Editor')
+        self.window.resize(500, 300)
+        #I create the layout for the window and the toolbar
+        window_layout = QVBoxLayout()
+        toolbar_layout = QHBoxLayout()
+        
+        #I create three buttons
+        button_open = QPushButton('Open')
+        button_save = QPushButton('Save')
+        button_exit = QPushButton('Exit')
+        button_list = [button_open, button_save, button_exit]
+        
+        #Add the buttons to the toolbar
+        for x in button_list: toolbar_layout.addWidget(x)
+        
+        #Add the toolbar to the window layout
+        window_layout.addLayout(toolbar_layout)
+        
+        #I create an area to edit the text
+        text_edit = QTextEdit('')
+        
+        #Add the text area to the window_layout
+        window_layout.addWidget(text_edit)
+        
+        #Set the window layout
+        self.window.setLayout(window_layout)
+        
+        #It define the callback for the respective buttons
+        def Open_Callback():
+            filename, _ = QFileDialog.getOpenFileName(self.window)
+            if not filename: return
+            with open(filename) as f: text_edit.setText(f.read())
+        
+        def Save_Callback():
+            filename, _ = QFileDialog.getSaveFileName(self.window)
+            if not filename: return
+            with open(filename, 'w') as f: f.write(text_edit.toPlainText())
+        
+        def Quit_Callback():
+            self.app.quit()
+        
+        #Set the callback for every button
+        button_open.clicked.connect(Open_Callback)
+        button_save.clicked.connect(Save_Callback)
+        button_exit.clicked.connect(Quit_Callback)
+    
+    def _run_app(self):
+        self.window.show()
+        self.app.exec_()
+    
+class QSDesktop(object):
+    def __init__(self, text_editor, web_browser):
+        app, window = BuildWindow()
+        self.app = app
+        self.window = window
+        self.text_editor = text_editor
+        self.web_browser = text_editor
+    
+    def _build_desktop(self):
+        self.window.setWindowTitle('Simple Desktop')
+        self.window.resize(700, 400)
+        window_layout = QHBoxLayout()
+        editor_button = QPushButton('Text Editor')
+        browser_button = QPushButton('Web Browser')
+        window_layout.addWidget(editor_button)
+        window_layout.addWidget(browser_button)
+        self.window.setLayout(window_layout)
+        
+        def Editor_Callback():
+            self.text_editor._build_editor()
+            self.text_editor._run_app()
+        
+        def Browser_Callback():
+            self.web_browser._build_browser()
+            self.web_browser._run_app()
+        
+        editor_button.clicked.connect(Editor_Callback)
+        browser_button.clicked.connect(Browser_Callback)
+        
+if __name__ == '__main__':
+    #I need of two object: one of the QWBrowser class and 
+    #one of the second class to pass as argument in the 
+    #QSDesktop class
+    text_editor = QTEditor()
+    web_browser = QWBrowser()
+    #I create an object "desktop" of the class QSDesktop
+    desktop = QSDesktop(text_editor, web_browser)
+    #Use the _build_desktop method, of the QSDesktop class to build the desktop
+    desktop._build_desktop()
+    #Use the runApp function to execute the window
+    runApp(desktop.app, desktop.window)
         
         
